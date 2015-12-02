55513beddc0500c36d26ce9e0f26b496b07acec6
张富强 <3448622@qq.com>
2015-12-02 19:19:28 +0800

    Initial Commit

---
+++ testdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testdemo
+//
+//  Created by zhangfuqiang on 15/12/2.
+//  Copyright © 2015年 zhangfuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testdemo
+//
+//  Created by zhangfuqiang on 15/12/2.
+//  Copyright © 2015年 zhangfuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
